69c0ce0056ad34e141b1cfdca6ef125d4d41c555
erezkalifa <erezkalifa10@gmail.com>
2025-03-25 12:33:11 +0200

    Initialize scss structure folders. Add firsts routes

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -2,6 +2,9 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "../src/assets/main.css";
 import { AppHeader } from "./cmps/AppHeader.jsx";
 import { HomePage } from "./pages/HomePage.jsx";
+import { GiftCards } from "./pages/GiftCards.jsx";
+import { HostWelcomePage } from "./pages/HostWelcomePage.jsx";
+import { HostExperience } from "./pages/HostExperience.jsx";
 
 export function RootCmp() {
   return (
@@ -15,6 +18,10 @@ export function RootCmp() {
               <Route index element={<Login />} />
               <Route path="signup" element={<Signup />} />
             </Route> */}
+            <Route path="/giftcards" element={<GiftCards />} />
+            <Route path="/help" element={<HelpCenter />} />
+            <Route path="/host/experiences" element={<HostExperience />} />
+            <Route path="/host/homes" element={<HostWelcomePage />} />
           </Routes>
         </main>
       </section>
