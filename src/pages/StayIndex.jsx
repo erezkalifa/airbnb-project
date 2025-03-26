73fc1f0910f07ed6179f8d63d76ae717684ae498
Ben Zion Fischer <benzionfisher@gmail.com>
2025-03-26 23:17:19 +0200

    Generate demo logged in user

--- src/pages/StayIndex.jsx
+++ src/pages/StayIndex.jsx
@@ -3,6 +3,10 @@ import { useSelector } from "react-redux";
 import { loadStays } from "../store/stay/stay.actions.js";
 import { StayPreview } from "../cmps/stayPreview.jsx";
 
+// Importing _createLoggedInUser for the sole purpose of creating the logged-in user if none exists ( demo data creation )
+// You can delete this line if someone else call one of the functions of user.service.js
+import { _createLoggedInUser } from "../services/user.service.js";
+
 export function StayIndex() {
   const stays = useSelector((storeState) => storeState.stayModule.stays);
   useEffect(() => {
