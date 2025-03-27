803057be08f32bdb503b17502d627489f614fb8e
Ben Zion Fischer <benzionfisher@gmail.com>
2025-03-27 18:33:46 +0200

    refactor comments

--- src/pages/StayIndex.jsx
+++ src/pages/StayIndex.jsx
@@ -3,8 +3,7 @@ import { useSelector } from "react-redux";
 import { loadStays } from "../store/stay/stay.actions.js";
 import { StayPreview } from "../cmps/stayPreview.jsx";
 
-// Importing _createLoggedInUser for the sole purpose of creating the logged-in user if none exists ( demo data creation )
-// You can delete this line if someone else call one of the functions of user.service.js
+// Demo data creation
 import { _createLoggedInUser } from "../services/user.service.js";
 import { _createOrders } from "../services/order.service.js";
 
