3983de3b2621a8904b37d14948c881369330074a
Ben Zion Fischer <benzionfisher@gmail.com>
2025-03-28 17:31:28 +0300

    filters responsiveness: Add MobileFilter

---
+++ src/cmps/MobileFilter.jsx
@@ -0,0 +1,3 @@
+export function MobileFilter() {
+    return (<div>MobileFilter</div>)
+}
--- src/pages/StayIndex.jsx
+++ src/pages/StayIndex.jsx
@@ -4,6 +4,8 @@ import { loadStays } from "../store/stay/stay.actions.js";
 import { StayPreview } from "../cmps/stayPreview.jsx";
 import { LabelsScrollerBar } from "../cmps/LabelsScrollerBar.jsx"
 import { UpperFilter } from "../cmps/UpperFilter.jsx"
+import { MiddleFilter } from "../cmps/MiddleFilter.jsx"
+import { MobileFilter } from "../cmps/MobileFilter.jsx"
 
 
 // Demo data creation
@@ -14,15 +16,33 @@ import { _createOrders } from "../services/order.service.js";
 export function StayIndex() {
   const [selectedLabel,setSelectedLabel] = useState("Countryside")
   const stays = useSelector((storeState) => storeState.stayModule.stays);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768); // Check if screen width is <= 768px
 
   useEffect(() => {
     loadStays()
     .catch((err) => console.log(err));
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768); // Update state on window resize
+  };
 
   return (
     <section className="stay-index">
-      <UpperFilter/>
+      {!isMobile ? (
+        <>
+          <UpperFilter />
+          <MiddleFilter />
+        </>
+      ) : (
+        <MobileFilter />
+      )}
       <LabelsScrollerBar selectedLabel={selectedLabel} onLabelSelect={(label) => {
           console.log(label.name)
           setSelectedLabel(label)
