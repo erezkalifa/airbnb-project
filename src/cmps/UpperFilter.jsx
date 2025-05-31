e2f3d0745dbf285f7e1438ed8c376c668ab43e6b
romi shoval <romishoval14@gmail.com>
2025-05-31 12:26:26 +0300

    changed logo

--- src/cmps/UpperFilter.jsx
+++ src/cmps/UpperFilter.jsx
@@ -1,20 +1,45 @@
+// import { Avatar } from "./Avatar.jsx";
+// import { SmallFilter } from "./SmallFilter.jsx";
+// import { StaysExperiencesFilter } from "./StaysExpriencesFilter.jsx";
+// import { ProfileButton } from "./ProfileButton.jsx";
+
+// export function UpperFilter({ isAtTop , onOpenFilter }) {
+//   return (
+//     <section className="upper-header">
+//       {/* Logo */}
+//       <div className="logo">
+//         <a href="/">
+//           <img src="src/assets/img/logo-with-text.svg" alt="Logo" />
+//         </a>
+//       </div>
+
+//       {/* Filter */}
+//       {isAtTop ? <StaysExperiencesFilter /> : <SmallFilter onOpenFilter={onOpenFilter} />}
+//       <ProfileButton />
+//     </section>
+//   );
+// }
 import { Avatar } from "./Avatar.jsx";
 import { SmallFilter } from "./SmallFilter.jsx";
 import { StaysExperiencesFilter } from "./StaysExpriencesFilter.jsx";
 import { ProfileButton } from "./ProfileButton.jsx";
 
-export function UpperFilter({ isAtTop , onOpenFilter }) {
+export function UpperFilter({ isAtTop, onOpenFilter }) {
   return (
     <section className="upper-header">
-      {/* Logo */}
       <div className="logo">
         <a href="/">
-          <img src="src/assets/img/logo-with-text.svg" alt="Logo" />
+          <img src="src/assets/img/logo-aircnc.png" alt="Aircnc Logo" />
+          <span>aircnc</span>
         </a>
       </div>
 
       {/* Filter */}
-      {isAtTop ? <StaysExperiencesFilter /> : <SmallFilter onOpenFilter={onOpenFilter} />}
+      {isAtTop ? (
+        <StaysExperiencesFilter />
+      ) : (
+        <SmallFilter onOpenFilter={onOpenFilter} />
+      )}
       <ProfileButton />
     </section>
   );
