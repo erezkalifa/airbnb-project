5e2a35b0a052fa2900efb1adfbf4f1d3570d1a28
erezkalifa <erezkalifa10@gmail.com>
2025-05-31 14:59:25 +0300

    fixed google map api bugs

--- src/cmps/MapView.jsx
+++ src/cmps/MapView.jsx
@@ -1,14 +1,51 @@
+import { useEffect, useState } from "react";
 import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
 
-export function MapView({ stay, style }) {
-  const center = {
-    lat: stay?.loc?.lan || 32.0835416152454,
-    lng: stay?.loc?.lat || 34.78832515996161,
-  };
+export function MapView({ stay, style, address }) {
+  const [location, setLocation] = useState({
+    lat: 32.0835416152454,
+    lng: 34.78832515996161,
+  });
+
+  useEffect(() => {
+    if (address) {
+      handleLocationByAddress();
+    } else {
+      handleLocationByStay();
+    }
+  }, [address, stay]);
+
+  function handleLocationByStay() {
+    if (!stay?.loc) return;
+    setLocation({
+      lat: stay.loc.lan,
+      lng: stay.loc.lat,
+    });
+  }
+
+  async function handleLocationByAddress() {
+    const encodedAddress = encodeURIComponent(address);
+    try {
+      const res = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCd6v2whmmM0grZKw0hD5n1ssBatDjzulc`
+      );
+
+      const data = await res.json();
+      if (data.status === "OK") {
+        const loc = data.results[0].geometry.location;
+        setLocation({ lat: loc.lat, lng: loc.lng });
+      } else {
+        console.error("Geocoding failed:", data.status);
+      }
+    } catch (err) {
+      console.error("Fetch error:", err);
+    }
+  }
+
   return (
     <APIProvider apiKey="AIzaSyCd6v2whmmM0grZKw0hD5n1ssBatDjzulc">
       <Map
-        center={center}
+        center={location}
         defaultZoom={13}
         mapId="2bd328edb13e41cede0051a4"
         mapOptions={{
@@ -17,14 +54,14 @@ export function MapView({ stay, style }) {
         }}
         style={style || { width: "100%", height: "500px" }}
       >
-        <AdvancedMarker position={center}>
+        <AdvancedMarker position={location}>
           <div
             style={{
               background: "#fff",
               borderRadius: "50%",
               width: "40px",
               height: "40px",
-              boxShadow: "0 2px 6px rgba(0,0,0,0.3)",
+              boxShadow: "0 2px 6px rgba(0, 0, 0, 0.3)",
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
--- src/pages/back-office/StayWizard/StepPlaceLocation.jsx
+++ src/pages/back-office/StayWizard/StepPlaceLocation.jsx
@@ -1,14 +1,102 @@
 import { MapView } from "../../../cmps/MapView.jsx";
-
+import { useState } from "react";
 export function StepPlaceLocation() {
+  const [placeDetails, setPlaceDetails] = useState({});
+  const [mapAddress, setMapAddress] = useState(null);
+
+  //   console.log(placeDetails);
+
+  function handleSearchBtn() {
+    const { street, city, postalCode, entrance } = placeDetails;
+
+    const fullAddress = buildAddressString({
+      street,
+      city,
+      postalCode,
+      entrance,
+    });
+
+    setMapAddress(fullAddress);
+  }
+
+  const buildAddressString = (addressObj) => {
+    return [addressObj.street, addressObj.city, addressObj.postalCode]
+      .filter(Boolean)
+      .join(", ");
+  };
+
   return (
     <section className="step-place-location">
-      <h2>Where's your place located?</h2>
-      <h3>
-        Your address is only shared with guests after they’ve made a
-        reservation.
-      </h3>
-      <MapView style={{ width: "630px", height: "500px" }} />
+      <div className="txt">
+        <div className="where">Where's your place located?</div>
+        <span>
+          Your address is only shared with guests after they’ve made a
+          reservation.
+        </span>
+      </div>
+      <MapView
+        style={{ width: "630px", height: "500px" }}
+        address={mapAddress}
+      />
+      <div className="airbnb-input-group">
+        <div className="input-row">
+          <input
+            type="text"
+            className="input-field"
+            placeholder="Street address"
+            onChange={(event) =>
+              setPlaceDetails((prev) => ({
+                ...prev,
+                street: event.target.value,
+              }))
+            }
+          />
+        </div>
+        <div className="input-row">
+          <input
+            type="text"
+            className="input-field"
+            placeholder="City/Town"
+            onChange={(event) =>
+              setPlaceDetails((prev) => ({ ...prev, city: event.target.value }))
+            }
+          />
+        </div>
+
+        <div className="input-row">
+          <input
+            type="text"
+            className="input-field"
+            placeholder="Postal code"
+            onChange={(event) =>
+              setPlaceDetails((prev) => ({
+                ...prev,
+                postalCode: event.target.value,
+              }))
+            }
+          />
+        </div>
+        <div className="input-row">
+          <input
+            type="text"
+            className="input-field"
+            placeholder="Entrance (if applicable)"
+            onChange={(event) =>
+              setPlaceDetails((prev) => ({
+                ...prev,
+                entrance: event.target.value,
+              }))
+            }
+          />
+        </div>
+      </div>
+      <button
+        className="option-box wide-btn"
+        style={{ "--btn-width": "550px" }}
+        onClick={handleSearchBtn}
+      >
+        Search
+      </button>
     </section>
   );
 }
