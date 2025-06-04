34d6cc095f138696096fd0d00431b45640a82bb9
romi shoval <romishoval14@gmail.com>
2025-06-04 21:37:14 +0300

    added to localHost - http

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -7,9 +7,9 @@ var axios = Axios.create({
 const STORAGE_KEY_LOGGEDIN_USER = "loggedinUser";
 
 const BASE_URL =
-  process.env.NODE_ENV !== "development" 
-  ? "https://airbnb-backend-egt6.onrender.com/api/" 
-  : "//localhost:3030/api/";
+  process.env.NODE_ENV == "development" 
+  ? "http://localhost:3030/api/"
+  : "https://airbnb-backend-egt6.onrender.com/api/" ;
 
 const BASE_USER_URL = BASE_URL + "user/";
 const BASE_AUTH_URL = BASE_URL + "auth/";
